a6c8270637a8ad533ee3b7a188a7fdb0b2488c67
LENOVO <hasabnissayali23@gmail.com>
2016-08-31 16:33:16 +0530

    sample line of demokkkkk

--- app/src/main/java/com/dsm/outgo/sample_svn/New.java
+++ app/src/main/java/com/dsm/outgo/sample_svn/New.java
@@ -1,7 +1,9 @@
 package com.dsm.outgo.sample_svn;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by LENOVO on 8/31/2016.
  */
-public class New {
+public class New extends AppCompatActivity{
 }
